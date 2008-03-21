828b89078d0be9fc8e7b3a9a08af75856985bf17
ski.whitespace <ski.whitespace@ac22f5f5-f148-0410-9719-5941d86a54b7>
2008-03-21 13:39:54 +0000

    added copyright

--- trunk/epydoc.el
+++ trunk/epydoc.el
@@ -1,8 +1,33 @@
 ;;; eypdoc.el -- epydoc-style comments
-;; Author Atsushi Suga
-;; Copyright (C) 2007-2008 Atsushi Suga
-;;; this code is based on doxygen.el written by Tom Emerson.
 
+;; Copyright (c) 2008, Atsushi Suga
+;; All rights reserved.
+;; 
+;; Redistribution and use in source and binary forms,
+;; with or without modification,
+;; are permitted provided that the following conditions are met:
+;; 
+;; * Redistributions of source code must retain the above copyright notice,
+;;   this list of conditions and the following disclaimer.
+;; * Redistributions in binary form must reproduce the above copyright notice,
+;;   this list of conditions and the following disclaimer in the documentation and/or
+;;   other materials provided with the distribution.
+;; * the names of its contributors may be used to endorse or promote products
+;;   derived from this software without specific prior written permission.
+;; 
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+;; BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+;; FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+;; IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+;; INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+;; (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+;; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+;; HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+;; STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+;;; this code is based on doxygen.el written by Tom Emerson.
 ;;; forrow is doxygen.el Copyright ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; doxygen.el --- doxygen-style comments
@@ -33,12 +58,27 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; end of doxygen.el Copyright ;;;
 
+;;; totorial ;;;
+
 ;; use as
-;; write your .emacs
+;; write to your .emacs
 ;; (require 'epydoc)
 ;; 
+;; If you want file comment, type forrow at beginning of your python code
+;; M-x epydoc-insert-file-comment
 ;;
-
+;; And if you want function comment,
+;; type forrow at beginning of function definition (e.g def blur(arg):)
+;; M-x epydoc-insert-function-comment
+;;
+;; and you can define your default copyright, project-name and author-name
+;; like forrow
+;;    (setq epydoc-copyright
+;;	       "BSD"))
+;;    (setq epydoc-project-name
+;;	       "my python project"))
+;;    (setq epydoc-author-name
+;;	       "Atsushi Suga"))
 
 ;; bugs
 ;; epydoc-insert-function-comment can't indent correctly if there are not any statement
